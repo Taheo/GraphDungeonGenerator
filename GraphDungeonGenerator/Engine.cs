8a7b19142a9dcf78984fa4a7e31fceb08663eba2
Taheo <segieta.michal@gmail.com>
2025-02-22 17:35:11 +0100

    initial commit

---
+++ GraphDungeonGenerator/Engine.cs
@@ -0,0 +1,23 @@
+﻿using QuickGraph;
+
+namespace GraphDungeonGenerator
+{
+    public class Engine
+    {
+        public AdjacencyGraph<int, Edge<int>> GenerateSimpleGraph(List<int> vertexList)
+        {
+            var graph = new AdjacencyGraph<int, Edge<int>>();
+            graph.AddVertexRange(vertexList);
+
+            foreach (var item in graph.Vertices)
+            {
+                if (item != graph.VertexCount)
+                {
+                    graph.AddEdge(new Edge<int>(item, item + 1));
+                }
+            }
+
+            return graph;
+        }
+    }
+}
---
+++ Tester/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var engine = new GraphDungeonGenerator.Engine().GenerateSimpleGraph(new List<int> {1,2,3});
+
+        Console.WriteLine($" V {engine.VertexCount}");
+        foreach (var item in engine.Vertices)
+        {
+            Console.WriteLine(item);
+        }
+
+        Console.WriteLine($" K {engine.EdgeCount}");
+        foreach (var item in engine.Edges)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
