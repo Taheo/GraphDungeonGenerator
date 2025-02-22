fd73cdcd2a17246072ebc079c8f8313ca9d7d586
Taheo <segieta.michal@gmail.com>
2025-02-22 21:08:07 +0100

    random graph generation added

--- GraphDungeonGenerator/Engine.cs
+++ GraphDungeonGenerator/Engine.cs
@@ -4,10 +4,13 @@ namespace GraphDungeonGenerator
 {
     public class Engine
     {
-        public AdjacencyGraph<int, Edge<int>> GenerateSimpleGraph(List<int> vertexList)
+        public AdjacencyGraph<int, Edge<int>> GenerateSimpleGraph(int vertices)
         {
             var graph = new AdjacencyGraph<int, Edge<int>>();
-            graph.AddVertexRange(vertexList);
+            for (int i = 1; i <= vertices; i++)
+            {
+                graph.AddVertex(i);
+            }
 
             foreach (var item in graph.Vertices)
             {
@@ -18,6 +21,60 @@ namespace GraphDungeonGenerator
             }
 
             return graph;
+        }
+
+        public AdjacencyGraph<int, Edge<int>> GenerateRandomGraphWithOneEntrance(AdjacencyGraph<int, Edge<int>> graphToRandomize)
+        {
+            var graph = new AdjacencyGraph<int, Edge<int>>();
+            var firstVertex = graphToRandomize.Vertices.First();
+            var random = new Random();
+
+            foreach (var vertex in graphToRandomize.Vertices)
+            {
+                graph.AddVertex(vertex);
+            }
+
+            var edges = graphToRandomize.Edges.ToList();
+            var secondVertex = graphToRandomize.Vertices.First() + 1;
+            var thirdVertex = graphToRandomize.Vertices.First() + 2;
+
+            graph.AddEdge(new Edge<int>(firstVertex, secondVertex));
+            graph.AddEdge(new Edge<int>(firstVertex, thirdVertex));
+            var rng = new Random();
+
+
+            foreach (var item in graphToRandomize.Vertices.Where(x => x != firstVertex))
+            {
+                graph.AddEdge(new Edge<int>(item, GenerateNextVertex(item, graphToRandomize.VertexCount)));
+            }
+
+            graph.RemoveEdge(graph.Edges.Last());
+
+            return graph;
+        }
+
+        private int GenerateNextVertex(int vertex, int max)
+        {
+            var rng = new Random();
+            int nextVert;
+
+
+            if (vertex + 1 == max)
+            {
+                return vertex + 1;
+            }
+
+            if (vertex == max)
+            {
+                return vertex;
+            }
+
+            do
+            {
+                nextVert = rng.Next(vertex, max);
+            } while (nextVert <= vertex);
+
+            return nextVert;
         }
     }
 }
--- Tester/Program.cs
+++ Tester/Program.cs
@@ -1,17 +1,25 @@
-﻿internal class Program
+﻿using QuickGraph;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        var engine = new GraphDungeonGenerator.Engine().GenerateSimpleGraph(new List<int> {1,2,3});
+        var engine = new GraphDungeonGenerator.Engine();
+        var simpleGraph = engine.GenerateSimpleGraph(10);
+        var randomGraph = engine.GenerateRandomGraphWithOneEntrance(simpleGraph);
+        DisplayGraph(randomGraph);
+    }
 
-        Console.WriteLine($" V {engine.VertexCount}");
-        foreach (var item in engine.Vertices)
+    private static void DisplayGraph(AdjacencyGraph<int, Edge<int>> graph)
+    {
+        Console.WriteLine($" V {graph.VertexCount}");
+        foreach (var item in graph.Vertices)
         {
             Console.WriteLine(item);
         }
 
-        Console.WriteLine($" K {engine.EdgeCount}");
-        foreach (var item in engine.Edges)
+        Console.WriteLine($" K {graph.EdgeCount}");
+        foreach (var item in graph.Edges)
         {
             Console.WriteLine(item);
         }
